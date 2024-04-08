ca0a687877d90d8ebef3a310ede93f65e3b5e3e2
Omar <omar-trevino@hotmail.com>
2024-04-08 13:33:34 -0600

    Prueba de onDelete Cascade de Recibos y Recibo_Itemas

--- database/migrations/2023_10_06_200313_create_recibo_items_table.php
+++ database/migrations/2023_10_06_200313_create_recibo_items_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('recibo_items', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId('recibo_id')->references('id')->on('recibos');
-            $table->foreignId('provedor_id')->references('id')->on('provedors');
+            $table->foreignId('recibo_id')->references('id')->on('recibos')->onDelete('cascade');;
+            $table->foreignId('provedor_id')->nullable()->references('id')->on('provedors')->nullOnDelete();
             $table->string('factura')->unique();
             $table->date('fecha_factura');
             $table->string('concepto');
